2fe606130f4481b9559c4edae4665b85f3b585a3
Allvaa <alvamailindo@gmail.com>
2020-11-14 16:20:27 +0700

    update seek command

--- src/commands/seek.js
+++ src/commands/seek.js
@@ -1,11 +1,12 @@
 const util = require("../util");
 
-const durationPattern = /^[0-5]?[0-9](:[0-5][0-9]+)$/;
+const durationPattern = /^[0-5]?[0-9](:[0-5][0-9]{2})$/;
 
 module.exports = {
     name: "seek",
     exec: async (msg, args) => {
         const { music } = msg.guild;
+        if (!music.player) return msg.channel.send("Currently not playing anything.");
         if (!msg.member.voice.channel)
             return msg.channel.send("You must be on a voice channel.");
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
@@ -13,13 +14,18 @@ module.exports = {
 
         const duration = args[0];
         if (!durationPattern.test(duration))
-            return msg.channel.send("Your inputted duration isn't valid. Valid duration eg. `1:34`.");
+            return msg.channel.send("You provided an invalid duration. Valid duration e.g. `1:34`.");
 
         const durationMs = util.durationToMillis(duration);
+        if (durationMs > music.current.info.length)
+            return msg.channel.send("The duration you provide exceeds the duration of the current track.");
+
         try {
             await music.player.seek(durationMs);
+            msg.channel.send(`Seeked to ${util.millisToDuration(durationMs)}.`);
         } catch (e) {
             msg.channel.send(`An error occured: ${e.message}`);
         }
     }
 };
+clearTimeout();
--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const prettyMilliseconds = require("pretty-ms");
 
 module.exports = {
     embed: () => {
@@ -7,5 +8,8 @@ module.exports = {
     },
     durationToMillis: (dur) => {
         return dur.split(":").map(Number).reduce((acc, curr) => curr + acc * 60) * 1000;
-    } 
+    },
+    millisToDuration: (ms) => {
+        return prettyMilliseconds(ms, { colonNotation: true, secondsDecimalDigits: 0 });
+    }
 };
