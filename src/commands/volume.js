21676249b80f17146cccfbca255cd4f06e5c2435
Allvaa <alvamailindo@gmail.com>
2020-12-16 15:26:33 +0700

    add volume limit

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -5,16 +5,19 @@ module.exports = {
     aliases: ["vol"],
     exec: async (msg, args) => {
         const { music } = msg.guild;
-        const newVolume = args[0];
+        const newVolume = parseInt(args[0], 10);
         if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
         try {
-            if (!newVolume || isNaN(newVolume)) {
+            if (isNaN(newVolume)) {
                 msg.channel.send(util.embed().setDescription(`🔉 | Current volume \`${music.volume}\`.`));
             } else {
                 if (!msg.member.voice.channel)
                     return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
                 if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
                     return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+
+                if (newVolume < 0 || newVolume > 150)
+                    return msg.channel.send(util.embed().setDescription("❌ | You can only set the volume from 0 to 150."));
 
                 await music.setVolume(newVolume);
                 msg.channel.send(util.embed().setDescription(`🔉 | Volume set to \`${music.volume}\`.`));
