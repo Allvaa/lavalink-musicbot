f03ab7076956eb5ca2c8895624c44866dd83ebb2
Allvaa <alvamailindo@gmail.com>
2021-01-13 23:54:54 +0700

    :eyes:

--- src/structures/Rest.js
+++ src/structures/Rest.js
@@ -6,7 +6,7 @@ module.exports = class Rest extends require("lavacord").Rest {
      * @returns {Promise<import("lavacord").TrackResponse>}
      */
     static async load(node, query, lsClient) {
-        const spotify = lsClient ? lsClient.getNode(node.id) : undefined;
+        const spotify = lsClient ? lsClient.nodes.get(node.id) : undefined;
         return lsClient && lsClient.isValidURL(query) ? await spotify.load(query) : await super.load(node, query);
     }
 };
