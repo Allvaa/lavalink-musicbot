5ebc07659d333f4a24ca97e2dfb03756d8146366
Allvaa <alvamailindo@gmail.com>
2021-02-06 23:58:57 +0700

    :thinking:

--- src/commands/move.js
+++ src/commands/move.js
@@ -5,8 +5,8 @@ module.exports = {
     aliases: ["mv"],
     exec: async (msg, args) => {
         const { music } = msg.guild;
-        const from = parseInt(args[0], 10);
-        const to = parseInt(args[1], 10);
+        const from = args[0] ? parseInt(args[0], 10) : null;
+        const to = args[1] ? parseInt(args[1], 10) : null;
         if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
         if (!music.queue.length) return msg.channel.send(util.embed().setDescription("❌ | Queue is empty."));
 
@@ -14,6 +14,9 @@ module.exports = {
             return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
             return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+
+        if (from === null || to === null)
+            return msg.channel.send(util.embed().setDescription(`❌ | Missing args. Example usage e.g. \`${msg.client.prefix}move 2 1\``));
 
         if (from === to || (isNaN(from) || from < 1 || from > music.queue.length) || (isNaN(to) || to < 1 || to > music.queue.length))
             return msg.channel.send(util.embed().setDescription("❌ | Number is invalid or exceeds queue length."));
