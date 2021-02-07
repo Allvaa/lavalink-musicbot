a67b4059ad03841a68be76fe38d11d03a40292cb
Allvaa <alvamailindo@gmail.com>
2021-02-07 13:49:09 +0700

    previous command

---
+++ src/commands/previous.js
@@ -0,0 +1,24 @@
+const util = require("../util");
+
+module.exports = {
+    name: "previous",
+    aliases: ["prev"],
+    exec: async (msg) => {
+        const { music } = msg.guild;
+        if (!music.player) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
+        if (!music.previous) return msg.channel.send(util.embed().setDescription("❌ | No previous track."));
+
+        if (!msg.member.voice.channel)
+            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+        if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
+            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+
+        try {
+            music.queue.unshift(music.previous);
+            await music.skip();
+            msg.react("⏮️").catch(e => e);
+        } catch (e) {
+            msg.channel.send(`An error occured: ${e.message}.`);
+        }
+    }
+};
