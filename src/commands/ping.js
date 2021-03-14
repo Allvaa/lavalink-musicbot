60b43aa7703ad739c4203f188ac51511a67df28e
LoneWeebGod <73281112+LoneWeebGod@users.noreply.github.com>
2021-03-14 17:29:32 +0530

    Better Ping CMD
    Get both the ping of the bot itself and discord API (Actually bot Ping on Discord)

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,16 @@
+const util = require("../util");
 module.exports = {
     name: "ping",
-    exec: (msg) => {
-        msg.channel.send(`🏓 Pong! \`${msg.client.ws.ping}ms\``);
-    }
+    exec: (msg) => {		
+    const latency = `\`\`\`ini\n ${Math.floor(Date.now() - msg.createdTimestamp)}ms \`\`\``;
+    const apiLatency = `\`\`\`ini\n ${Math.round(msg.client.ws.ping)}ms \`\`\``;
+         msg.channel.send(util.embed()
+		.setAuthor(`OKAY (。_。)` , msg.client.user.displayAvatarURL())		
+		.setTitle(`Ping Results -`)
+		.setDescription('')
+        .addField('Latency', latency, true)
+        .addField('API Latency', apiLatency, true)
+		.setFooter('(x_x)')
+		.setTimestamp());
+}
 };
