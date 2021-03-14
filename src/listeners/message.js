76bf135af437e6f5cb076a5d2ffdc3153af7aefe
LoneWeebGod <73281112+LoneWeebGod@users.noreply.github.com>
2021-03-14 18:20:27 +0530

    Use the Custom Prefix
    Will use the custom prefix of the server!

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -1,11 +1,27 @@
+const { readFileSync } = require("fs");
+
+const fs = require("fs");
+const Prefix = process.env.PREFIX;
+
 module.exports = {
     name: "message",
     exec: async (client, msg) => {
         if (!msg.guild) return;
         if (msg.author.bot) return;
-        if (!msg.content.startsWith(client.prefix)) return;
+        
+        let prefixes = JSON.parse(fs.readFileSync('./prefixes.json', "utf8"));
+		
+         if(!prefixes[msg.guild.id]){
+			 prefixes[msg.guild.id] = {
+				 prefix: process.env.PREFIX
+			 }
+		 }
 
-        const args = msg.content.slice(client.prefix.length).trim().split(/ +/g);
+		  let prefix  = prefixes[msg.guild.id].prefix;
+        
+        if (!msg.content.startsWith(prefix)) return;
+
+        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase();
         const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));
         if (command) {
