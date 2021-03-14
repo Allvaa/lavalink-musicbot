218da89e757e52e1760ced1506a3e52f5b1683af
LoneWeebGod <73281112+LoneWeebGod@users.noreply.github.com>
2021-03-14 17:36:37 +0530

    Mentions as Prefix
    One can mention the bot for executing the command if they didn't know the Prefix. isn't that just awesome? （￣︶￣）↗　

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -2,9 +2,25 @@ module.exports = {
     name: "message",
     exec: async (client, msg) => {
         if (!msg.guild) return;
-        if (msg.author.bot) return;
+        if (msg.author.bot) return;     
+       
+        const senpai = `<@!${client.user.id}>`
+		if (msg.content.toLowerCase().startsWith(senpai)) {			
+const args = msg.content.slice(senpai.length).trim().split(/ +/g);	        
+        const commandName = args.shift().toLowerCase();		
+        const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));		
+        if (command) {
+            try {
+                await command.exec(msg, args);
+            } catch (e) {
+                console.error(e);
+            }
+			}
+		};
+        
+        
         if (!msg.content.startsWith(client.prefix)) return;
-
+        
         const args = msg.content.slice(client.prefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase();
         const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));
