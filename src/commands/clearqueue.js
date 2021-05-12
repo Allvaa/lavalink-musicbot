cee4541d6fbfe0a0eba567718bf3700d86c7cd74
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-05-12 23:57:48 +0530

    add clear queue command (#89)

---
+++ src/commands/clearqueue.js
@@ -0,0 +1,25 @@
+const util = require("../util");
+
+module.exports = {
+    name: "clearqueue",
+    description:"Clears The Total Queue.",
+    aliases: ["clr","clear"],
+    exec: (msg) => {
+        const { music } = msg.guild;
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌|  Currently not playing anything."));
+        if (!music.queue.length) return msg.channel.send(util.embed().setDescription("❌ | Queue is empty."));
+
+        if (!msg.member.voice.channel)
+            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+        if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
+            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+        try {
+            let first = music.queue[0];
+            music.queue = []
+            music.queue.push(first);
+            msg.channel.send(util.embed().setDescription("✅ | cleared The Queue")).catch(e => e);
+        } catch (e) {
+            msg.channel.send(`An error occured: ${e.message}.`);
+        }
+    }
+};
