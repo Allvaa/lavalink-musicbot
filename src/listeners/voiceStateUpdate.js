a6bfddaff8b717322a2b5d13098e77c04ac20103
mina <28566705+mina9999@users.noreply.github.com>
2021-06-04 13:52:11 +0430

    fix when member not cached (#105)

--- src/listeners/voiceStateUpdate.js
+++ src/listeners/voiceStateUpdate.js
@@ -1,6 +1,9 @@
 module.exports = {
     name: "voiceStateUpdate",
     exec: async (client, oldState, newState) => {
+        // if the member was not cached
+        if (!newState.member) await newState.guild.members.fetch(newState.id);
+
         const { guild: { music } } = newState;
         if (newState.member.user.equals(client.user) && !newState.channel && music.player) {
             if (music.player.playing) await music.stop();
