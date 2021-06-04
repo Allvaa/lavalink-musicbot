c837bbfe0d9c11a6236ac59d8d0acf02c9c7e328
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-06-04 14:41:21 +0530

    Create vapowave.js (#98)

---
+++ src/commands/vapowave.js
@@ -0,0 +1,18 @@
+const util = require("../util");
+module.exports = {
+    name: "vapowave",
+    aliases: ["vop"],
+    exec: async (msg, args) => {
+
+        const { music } = msg.guild;
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
+        if (!msg.member.voice.channel)
+            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+        if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
+            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`)); 
+     
+        music.setVaporwave(!music.vaporwave);  
+        msg.channel.send(util.embed().setDescription(` ${music.vaporwave ? "✅ | **enabled**" : "❌ | **disabled**"} Vaporwave`));
+
+    }
+};
