4b5de9c066e8229135b0deb75c9830c60afd228d
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2021-06-05 22:13:15 +0530

    automatic leave
    leaves the vc in 45 mins if playing and alone and in 30 mins if still in vc and nothing being played~

--- src/listeners/voiceStateUpdate.js
+++ src/listeners/voiceStateUpdate.js
@@ -9,5 +9,28 @@ module.exports = {
             if (music.player.playing) await music.stop();
             if (music.player) await client.manager.leave(music.guild.id);
         }
+         if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel)
+    return;
+        
+         if (!oldState.channel.members.size - 1 && !music.player.playing && music.player) {
+    setTimeout(() => { 
+      if (!oldState.channel.members.size - 1 && !music.player.playing && music.player)   
+      client.manager.leave(music.guild.id); 
+  music.textChannel.send(util.embed().setDescription(`I wasn't Playing Anything And No One Was there So I left.`)
+					.setFooter(music.guild.name, music.guild.iconURL({ dynamic: true }))
+				.setTimestamp());
+      music.reset();
+     }, 600000);
+  } else if (!oldState.channel.members.size - 1 && music.player.playing && music.player) {
+    setTimeout(() => { 
+      if (!oldState.channel.members.size - 1 && music.player.playing && music.player) 	  
+      client.manager.leave(music.guild.id);
+         music.textChannel.send(util.embed().setDescription(`No One Was In VC So I left.`)
+					.setFooter(music.guild.name, music.guild.iconURL({ dynamic: true }))
+				.setTimestamp());
+      music.reset();
+     }, 1800000); 
+  } 
     }
+    
 };
