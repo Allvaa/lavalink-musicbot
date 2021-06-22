c6eccb4cc9b459b98c6ff2375b13faba0e1903b3
iMu7aMMaD <69051329+iMu7aMMaD@users.noreply.github.com>
2021-06-22 16:18:46 +0200

    Replay with prefix when bot mentioned.

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -4,10 +4,15 @@ module.exports = {
         if (!msg.guild) return;
         if (msg.author.bot) return;     
 
-        const prefix = msg.content.startsWith(client.prefix) ? client.prefix : `<@!${client.user.id}>`;
-        if (!msg.content.startsWith(prefix)) return;
+    const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+    const newPrefix = msg.content.match(prefixMention) ? msg.content.match(prefixMention)[0]: client.prefix;
+
+    const getPrefix = new RegExp(`^<@!?${client.user.id}>( |)$`); 
+        if (msg.content.match(getPrefix)) return msg.channel.send(`Current Prefix: \`${client.prefix}\`.`);
+        if (msg.content.indexOf(newPrefix) !== 0) return;
+
+    const args = msg.content.slice(newPrefix.length).trim().split(/ +/g);
         
-        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase();
         const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));
         if (command) {
