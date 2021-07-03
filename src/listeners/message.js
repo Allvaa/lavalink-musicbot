3f3d6e323cb142f4ed4e984a7bb2c115b3fe4c62
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2021-07-03 06:01:49 -0700

    Makes the prefix case insensitive

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -4,8 +4,8 @@ module.exports = {
         if (!msg.guild) return;
         if (msg.author.bot) return;     
 
-        const prefix = msg.content.startsWith(client.prefix) ? client.prefix : `<@!${client.user.id}>`;
-        if (!msg.content.startsWith(prefix)) return;
+        const prefix = msg.content.toLowerCase().startsWith(client.prefix) ? client.prefix : `<@!${client.user.id}>`;
+        if (!msg.content.toLowerCase().startsWith(prefix)) return;
         
         const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase();
