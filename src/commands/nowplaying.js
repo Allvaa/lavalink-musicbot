ffa6c09046ffef609b190f2df04242cd14af2fd1
Allvaa <alvamailindo@gmail.com>
2021-08-21 15:42:48 +0700

    fix nowplaying command

--- src/commands/nowplaying.js
+++ src/commands/nowplaying.js
@@ -6,9 +6,9 @@ module.exports = {
     exec: (ctx) => {
         const { music } = ctx;
         if (!music.player?.track) return ctx.respond({ embeds: [util.embed().setDescription("❌ | Currently not playing anything.")] });
-        const progress = util.progress(music.player.state.position, music.current.info.length);
+        const progress = util.progress(music.state.position, music.current.info.length);
         ctx.respond({
-            embeds: [util.embed().setDescription(`🎶 | Now playing ${music.current.info.isStream ? "[**◉ LIVE**]" : ""}\n**${music.current.info.title}**.${music.current.info.isStream ? "" : `\n\n${util.millisToDuration(music.player.state.position)} ${progress.bar} ${util.millisToDuration(music.current.info.length)}`}`)]
+            embeds: [util.embed().setDescription(`🎶 | Now playing ${music.current.info.isStream ? "[**◉ LIVE**]" : ""}\n**${music.current.info.title}**.${music.current.info.isStream ? "" : `\n\n${util.millisToDuration(music.state.position)} ${progress.bar} ${util.millisToDuration(music.current.info.length)}`}`)]
         });
     }
 };
