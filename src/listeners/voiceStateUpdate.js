e3dc7455e288a407a2f16dba8261022e3515f6c8
Allvaa <alvamailindo@gmail.com>
2021-08-21 16:39:32 +0700

    fix manual disconnect

--- src/listeners/voiceStateUpdate.js
+++ src/listeners/voiceStateUpdate.js
@@ -3,8 +3,8 @@ module.exports = {
     exec: async (client, oldState, newState) => {
         const music = client.musics.get(newState.guild.id);
         if (newState.member.user.equals(client.user) && !newState.channel && music?.player) {
-            if (music.player.playing) await music.stop();
-            if (music.player) await client.manager.leave(music.guild.id);
+            if (music.player.track) await music.stop();
+            if (music.player) await music.node.leaveChannel(music.guild.id);
         }
     }
 };
