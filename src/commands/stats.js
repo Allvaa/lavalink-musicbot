c083e7b07f79664ad10c253b4f87254967fe6c21
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2021-08-26 06:40:30 +0530

    More stats
    Add server-side stats also along with lava link.

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,33 +1,37 @@
 const prettyMs = require("pretty-ms");
 const util = require("../util");
-
+const { mem, cpu} = require("node-os-utils");
 module.exports = {
     name: "stats",
     description: "Check nodes stats",
-    exec: (ctx) => {
+    exec: async (ctx) => {
         /** @type {import("lavacord").LavalinkNode[]} */
         const nodes = [...ctx.client.shoukaku.nodes.values()];
-
+        const { totalMemMb, usedMemMb } = await mem.info();
+        const serverStats = `\`\`\`asciidoc
+CPU  :: ${cpu.model()}
+Cores :: ${cpu.count()}
+CPU Usage :: ${await cpu.usage()} %
+RAM :: ${totalMemMb} MB
+RAM Usage :: ${usedMemMb} MB \`\`\``;        
+        const [lavalink] = nodes.map(node  => {
+            const cpuLoad = (node.stats.cpu.lavalinkLoad * 100).toFixed(2);
+            const memUsage = (node.stats.memory.used / 1024 / 1024).toFixed(2);
+            const uptime = prettyMs(node.stats.uptime, { verbose: false, secondsDecimalDigits: 0 });
+            return `\`\`\`asciidoc
+Lavalink ID        :: ${node.name.toUpperCase()}
+Lavalink Status    :: ${node.state === 1 ? "Connected" : "Disconnected"}
+${node.state === 1 ? `
+CPU Load :: ${cpuLoad}%
+Memory Usage :: ${memUsage} MB
+Lavalink Player Uptime :: ${uptime}
+Lavalink Players :: ${node.stats.playingPlayers} of ${node.stats.players} Playing` : ""}\`\`\``;});        
         ctx.respond({ embeds: [util.embed()
             .setAuthor("Lavalink Node(s) Stats", ctx.client.user.displayAvatarURL())
             .setTitle("Source Code")
             .setURL("https://github.com/Allvaa/lavalink-musicbot")
-            .setDescription(
-                nodes.map(node  => {
-                    const cpuLoad = (node.stats.cpu.lavalinkLoad * 100).toFixed(2);
-                    const memUsage = (node.stats.memory.used / 1024 / 1024).toFixed(2);
-                    const uptime = prettyMs(node.stats.uptime, { verbose: true, secondsDecimalDigits: 0 });
-
-                    return `\`\`\`asciidoc
-ID        :: ${node.name}
-Status    :: ${node.state === 1 ? "Connected" : "Disconnected"}
-${node.state === 1 ? `
-CPU Load  :: ${cpuLoad}%
-Mem Usage :: ${memUsage} MB
-Uptime    :: ${uptime}
-Players   :: ${node.stats.playingPlayers} of ${node.stats.players} playing` : ""}\`\`\``;
-                }).join("\n")
-            )
+            .addField("Server",serverStats)
+            .addField("Lavalink",lavalink)
             .setTimestamp()
         ] });
     }
